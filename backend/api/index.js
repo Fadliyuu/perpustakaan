ca0dc30a5978783f9ad96e314fedae3ecef35e64
Fadliyuu <fadliiskandar300@gmail.com>
2025-12-05 23:04:11 +0700

    Clean up merge conflict markers in api/index.js

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Vercel Serverless Function Entry Point
 // File ini untuk deploy backend di Vercel sebagai serverless function
 
@@ -10,22 +9,3 @@ module.exports = (req, res) => {
   // Delegate semua request ke Express app
   return app(req, res);
 };
-
-
-
-=======
-// Vercel Serverless Function Entry Point
-// File ini untuk deploy backend di Vercel sebagai serverless function
-
-const app = require('../src/app');
-
-// Export sebagai serverless function untuk Vercel
-// Vercel akan memanggil function ini untuk setiap request
-module.exports = (req, res) => {
-  // Delegate semua request ke Express app
-  return app(req, res);
-};
-
-
-
->>>>>>> bcc5009c16ab32ea931ef124fa91490e32799e85
