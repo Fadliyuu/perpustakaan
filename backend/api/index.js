a5a400bc7d2d1bb1dc0504a47c48ea406e90dd52
Fadliyuu <fadliiskandar300@gmail.com>
2026-04-07 11:40:03 +0700

    Update index.js

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,12 +1,3 @@
-// Vercel Serverless Function Entry Point
-// File ini untuk deploy backend di Vercel sebagai serverless function
-
 const app = require('../src/app');
 
-// Export sebagai serverless function untuk Vercel
-// Vercel akan memanggil function ini untuk setiap request
-module.exports = (req, res) => {
-  // Delegate semua request ke Express app
-  return app(req, res);
-};
-const app = require('../src/app'); module.exports = app;
+module.exports = app;
